bf957dd62c2b70e70e6f09fbdfdb4628caf4b227
Pream <preamqueentwesige@gmail.com>
2025-07-30 05:23:21 +0300

    javascript things

---
+++ weekone/datatypeandvariables.js
@@ -0,0 +1,55 @@
+/*
+Data types
+1.Strings/Text
+2.numbers
+integers
+big integer
+decimals
+3.boolean
+  true/false
+
+  4.undefined
+  5.null
+  let x-null
+  6.symbol
+   uniqueness
+   */
+  let productName = "timber"//strings we put them in Quotes
+  let productCost = 3500
+  let transportIsNeeded = true
+  let tableColor
+  let discount = null
+  console.log(productCost*2)
+  /*
+  Non primitive Data type
+  1.Array
+   is a list of items
+   [numbers]
+   [2,4,2.0]
+[strings]
+["sofa","table"]
+
+2. object
+in Javascript everything is an object
+productDescription{
+name:"sofa"
+price:2000
+color:"blue"
+}
+3. Date
+4. Function
+
+*/
+let productlist = ["sofa","table","cupboard"]
+console.log(productlist)
+
+let diningTableObject = {
+    productName: "Dining Table",
+    quantity:2,
+    price:350,
+    totalCost:quantity *  price
+}
+console.log(totalCost)
+let paymentType = "cash"
+const VAT = 0.05
+const PI = 3.14
---
+++ weekone/index.js
@@ -0,0 +1,17 @@
+let b=18;
+let c=20;
+let sum= b+c;
+console.log(sum)
+
+
+console.log("Welcome to the Javascript World!");
+
+let x=12;
+let y=3;
+let diff=y-x;
+console.log(diff)
+
+let man=2;
+let woman=2;
+let product=man*woman;
+console.log(product)
